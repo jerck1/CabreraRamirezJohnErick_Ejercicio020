af0a5b990914a93d131ef4dcbcefa4d254389f61
jerck1 <johncafre@gmail.com>
2020-04-03 12:08:39 -0500

    pushing

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # JohnErickCabreraRamirez_Ejercicio020
 03/04/2020
 03/04/2020
+03/04/2020
--- umap.py
+++ umap.py
@@ -62,33 +62,54 @@ print(np.shape(X))
 print(np.shape(Y))
 
 
-# In[18]:
+# In[43]:
 
 
-plt.figure(1,figsize = (12,12))
 cnt_2=0
+embedding_best=np.zeros((1137,2,4))
+
+
+# In[45]:
+
+
+plt.figure(1,figsize = (14,14))
 for i in range(2,12,2):
     for j in np.arange(0,1,0.2):
         reducer = umap.UMAP(n_neighbors=i, min_dist=j, metric='euclidean')#, n_components=2)
         reducer.fit(X)
         embedding = reducer.transform(X)
         np.shape(embedding)   
-        if (i==10):
-            embedding_best=embedding
+        if (2<=i<=8 and j==0.2):
+            embedding_best[:,:,int(i/2)-1]=embedding
         np.shape(embedding)
         cnt_2+=1
         plt.subplot(5,5,cnt_2)
         plt.scatter(embedding[:,0], embedding[:,1], c=Y, cmap='Paired', s=2.0)
         #plt.colorbar(boundaries=Y-0.5).set_ticks(np.arange(0,cnt,50))#(Y)
         plt.colorbar(boundaries=np.arange(0,cnt,100)-50).set_ticks(np.arange(0,cnt,100))
-        plt.title("neibr={}, d_min={}".format(i,j))
+        plt.title("neibr={}, d_min={:.1f}".format(i,j))
 plt.savefig("umap.png")
 
 
-# In[19]:
+# # Clustering sobre neibr={2,4,6,8}, d_min=0.2
+
+# In[ ]:
 
 
-#Clustering sobre
+a=embedding_best
+for i in range(4):
+    plt.subplot(4,1,i+1)
+    plt.scatter(a[:,0,i], a[:,1,i], c=Y, cmap='Paired', s=2.0)
+        #plt.colorbar(boundaries=Y-0.5).set_ticks(np.arange(0,cnt,50))#(Y)
+    plt.colorbar(boundaries=np.arange(0,cnt,100)-50).set_ticks(np.arange(0,cnt,100))
+    plt.title("neibr={}".format(i))
+
+
+# In[40]:
+
+
+print(np.shape(embedding_best))
+print(1137*2*4)
 
 
 # In[ ]:
@@ -97,31 +118,25 @@ plt.savefig("umap.png")
 #n_clusters = 10
 #Esto se hace para perplexity=10
 plt.figure(3,figsize = (10,20))
-for n_clusters in range(1,11):
-    k_means = sklearn.cluster.KMeans(n_clusters=n_clusters)
-    k_means.fit(embedding_best) # training
-    cluster = k_means.predict(embedding_best) # predice a cual cluster corresponde cada elmento
-    distance = k_means.transform(embedding_best) # calcula la distancia de cada elemento al centro de su cluster
-    plt.subplot(5,2,n_clusters)
-    plt.scatter(embedding_best[:,0], embedding_best[:,1], c=cluster, cmap='Paired', s=1.0)
-    plt.colorbar(boundaries=np.arange(11)-0.5).set_ticks(np.arange(n_clusters))
+for i in range(1):
+    for n_clusters in range(1,11):
+        k_means = sklearn.cluster.KMeans(n_clusters=n_clusters)
+        k_means.fit(embedding_best[:,:,i]) # training
+        cluster = k_means.predict(embedding_best[:,:,i]) # predice a cual cluster corresponde cada elmento
+        distance = k_means.transform(embedding_best[:,:,i]) # calcula la distancia de cada elemento al centro de su cluster
+        plt.subplot(5,2,n_clusters)
+        plt.scatter(embedding_best[:,0,i], embedding_best[:,1,i], c=cluster, cmap='Paired', s=1.0)
+        plt.colorbar(boundaries=np.arange(11)-0.5).set_ticks(np.arange(n_clusters))
 plt.savefig("clusters.png")
 
 
-# In[ ]:
+# In[27]:
 
 
-c=0
-for i in range(2,12,1):
-#    for j in np.arange(0,1,0.1):
-    c+=1
-    print(c)
-
-
-# In[ ]:
-
-
-np.arange(0,1,0.1)
+for i in range(2,12,2):
+    for j in np.arange(0,1.2,0.2):
+        if (2<=i<=8 and j==0.2):
+            print(i)
 
 
 # In[ ]:
